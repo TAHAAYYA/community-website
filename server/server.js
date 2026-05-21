3aff9e4b1d9448b4ece2fec4d52b85b10e01c54b
TAHAAYYA <tahaayyapvt@gmail.com>
2026-05-21 18:40:09 +0530

    Fix final Vercel CORS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://community-website-six.vercel.app"
+      "https://community-website-tahaayyas-projects.vercel.app"
     ],
     credentials: true
   })

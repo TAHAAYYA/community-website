d593586278bc9db8870cea695a45f87532379539
TAHAAYYA <tahaayyapvt@gmail.com>
2026-05-21 21:40:22 +0530

    Fix CORS https origin

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "community-website-ivory.vercel.app"
+      "https://community-website-ivory.vercel.app"
     ],
     credentials: true
   })
